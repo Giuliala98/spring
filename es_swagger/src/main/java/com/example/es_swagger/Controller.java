ba80f5a3129fbd302bdd753a04d167c0a60303b1
Giulia Lanza <giulia.lanza9816@gmail.com>
2024-04-15 17:53:23 +0200

    esercizio swagger svolto

--- es_swagger/src/main/java/com/example/es_swagger/Controller.java
+++ es_swagger/src/main/java/com/example/es_swagger/Controller.java
@@ -1,17 +1,21 @@
 package com.example.es_swagger;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
+
 public class Controller {
 
     @GetMapping("/name")
+    @Operation(summary = "Stampa il nome")
     public String name(@RequestParam String name) {
         return name;
     }
 
     @PostMapping("/reverseName")
+    @Operation(summary = "Stampa il nome al contrario")
     public String reverseName(@RequestParam String name) {
         StringBuilder reversedName = new StringBuilder(name);
         return reversedName.reverse().toString();
