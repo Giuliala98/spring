17ab57240bc385c18cca5cb041d0919dd2f7754a
Giulia Lanza <giulia.lanza9816@gmail.com>
2024-04-17 20:16:10 +0200

    esercizio operazioni crud svolto

---
+++ es_crud/src/main/java/com/example/es_crud/entity/Car.java
@@ -0,0 +1,20 @@
+package com.example.es_crud.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String modelName;
+
+    private String type;
+}
---
+++ es_crud/src/main/java/com/example/es_crud/repository/CarRepository.java
@@ -0,0 +1,9 @@
+package com.example.es_crud.repository;
+
+import com.example.es_crud.entity.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarRepository extends JpaRepository<Car, Long> {
+}
