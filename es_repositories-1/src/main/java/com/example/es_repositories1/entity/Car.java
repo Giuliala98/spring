51668bcd4ea0c9115db58932ad803d34299a35f1
Giulia Lanza <giulia.lanza9816@gmail.com>
2024-04-25 00:42:03 +0200

    corretto es repositories 1

--- es_repositories-1/src/main/java/com/example/es_repositories1/entity/Car.java
+++ es_repositories-1/src/main/java/com/example/es_repositories1/entity/Car.java
@@ -11,6 +11,7 @@ import java.math.BigDecimal;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table
 public class Car {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- es_repositories-1/src/main/java/com/example/es_repositories1/repository/CarRepository.java
+++ es_repositories-1/src/main/java/com/example/es_repositories1/repository/CarRepository.java
@@ -1,9 +1,9 @@
 package com.example.es_repositories1.repository;
 
+import com.example.es_repositories1.entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.stereotype.Repository;
 
 @RepositoryRestResource
-public interface Car extends JpaRepository<Car, Long> {
+public interface CarRepository extends JpaRepository<Car, Long> {
 }
